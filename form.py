a9e22352fff6a8de5e0505ef744a345f04ac7cfa
yokote2025s513-eng <yokote2025s513@e-akita.ed.jp>
2026-04-09 13:01:39 +0000

    fix form

---
+++ form.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+@app.ruote("/")
+def home():
+    return """
+    <h1>アンケート</h1>
+    <form action="/submit" method="post">
+         名前:<br>
+         <input type="text" name="name"><br><br>
+         <input type="submit" value="送信">
+    </form>
+    """
+
+    @app.route("/submit",methods=["POST"])
+    def submit():
+    name = request.form["name"]
+
+    print(name)
+
+    return f"送信ありがとう{name}!"
+port = int(os.environ.get("PORT",10001))
+app.run(host="0.0.0.0",port=port)
