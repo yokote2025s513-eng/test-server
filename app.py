d09b0901fa328ba4a81fda006497e0ea2863e34f
yokote2025s513-eng <yokote2025s513@e-akita.ed.jp>
2026-04-09 12:10:03 +0000

    fix port

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import flask
+from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -6,4 +7,5 @@ app = Flask(__name__)
 def home():
     return "server running"
 
-app.run(host="0.0.0.0",port=10000)
+port = int(os.environ.get("PORT",10000))
+app.run(host="0.0.0.0",port=port)
