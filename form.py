afe4bc63d3e2b5b12ba27a2ae065b646b9b06c23
yokote2025s513-eng <yokote2025s513@e-akita.ed.jp>
2026-04-12 23:27:41 +0000

    size form

--- form.py
+++ form.py
@@ -9,14 +9,18 @@ GAS_URL = "https://script.google.com/macros/s/AKfycbyL0O1jPIiSb_kLNUAkgywSYazZJK
 @app.route("/")
 def home():
     return """
-    <h1>背ネーム入力フォーム</h1>
+    <h1>サイズ解答フォーム</h1>
     <form action="/submit" method="post">
          名前:<br>
          <input type="text" name="name"><br><br>
-         出席番号:<br>
-         <input type="number" name="student_id"><br><br>
-         背ネーム:<br>
-         <input type="text" name="word"><br><br>
+         ウィンドブレーカ上:<br>
+         <select name="size1">
+         <option value="S">S</option>
+         <option value="M">M</option>
+         <option value="L">L</option>
+         <option value="XL">XL</option>
+         </select><br><br>
+         
          背番号:<br>
          <input type="number" name="number">
          <input type="submit" value="送信">
